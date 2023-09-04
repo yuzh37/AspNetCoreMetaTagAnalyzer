f360154dbcfc0c2fb752a97c149bd8c40f53488d
yuzh37 <yusoffzhang@gmail.com>
2023-09-04 16:11:54 +0800

    first commit

---
+++ MetaTagAnalyzerSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MetaTagAnalyzerSolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
